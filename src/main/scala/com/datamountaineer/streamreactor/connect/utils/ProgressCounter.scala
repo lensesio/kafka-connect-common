279053d1c433c151a45ace26dd451a83e01a4bdf
Antonios <antwnis@gmail.com>
2017-03-22 12:50:21 +0000

    Fixing progress counter

--- src/main/scala/com/datamountaineer/streamreactor/connect/utils/ProgressCounter.scala
+++ src/main/scala/com/datamountaineer/streamreactor/connect/utils/ProgressCounter.scala
@@ -29,7 +29,7 @@ import scala.collection.mutable
   * Created by andrew@datamountaineer.com on 03/03/2017. 
   * kafka-connect-common
   */
-case class ProgressCounter(periodMillis: Int) extends StrictLogging {
+case class ProgressCounter(periodMillis: Int = 60000) extends StrictLogging {
   private val startTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())
   private var timestamp: Long = 0
   private val counter = mutable.Map.empty[String, Long]
@@ -37,13 +37,15 @@ case class ProgressCounter(periodMillis: Int) extends StrictLogging {
   def update[T <: ConnectRecord[T]](records: Seq[ConnectRecord[T]]): Unit = {
     val newTimestamp = System.currentTimeMillis()
 
-    records.foreach(r => counter.put(r.topic() , counter.getOrElse(r.topic(), 0L) + 1L))
+    records.foreach(r => counter.put(r.topic(), counter.getOrElse(r.topic(), 0L) + 1L))
 
-    if (counter.nonEmpty && (newTimestamp - timestamp) >= 60000 && records.nonEmpty) {
-      counter.foreach( { case (k,v) => logger.info(s"Delivered $v records for $k since $startTime") })
+    if (counter.nonEmpty && (newTimestamp - timestamp) >= periodMillis && records.nonEmpty) {
+      counter.foreach({ case (k, v) => logger.info(s"Delivered $v records for $k since $startTime") })
       counter.empty
       timestamp = newTimestamp
     }
   }
+
+  def empty(): Unit = counter.clear()
 
 }
